699826e78f589953789d0199e5e601f8b846f60e
wiseman <jjwiseman@gmail.com>
2015-09-06 20:16:36 -0700

    Added schema for FAA registration database.

--- orbital.sql
+++ orbital.sql
@@ -34,3 +34,42 @@ CREATE TABLE IF NOT EXISTS aircraft_info (
 );
 
 CREATE INDEX aircraft_info_icao_index on aircraft_info (icao);
+
+CREATE TABLE IF NOT EXISTS faa_registration (
+       n_number VARCHAR(6),
+       serial_number VARCHAR(30),
+       mfr_mdl_code VARCHAR(7),
+       eng_mfr_mdl VARCHAR(5),
+       year_mfr VARCHAR(4),
+       type_registrant VARCHAR(1),
+       name varchar(50),
+       street VARCHAR(33),
+       street2 VARCHAR(33),
+       city VARCHAR(18),
+       state VARCHAR(2),
+       zip_code VARCHAR(10),
+       region VARCHAR(1),
+       county VARCHAR(3),
+       country VARCHAR(2),
+       last_action_date VARCHAR(8),
+       cert_issue_date VARCHAR(8),
+       certification VARCHAR(10),
+       type_aircraft VARCHAR(1),
+       type_engine VARCHAR(2),
+       status_code VARCHAR(2),
+       mode_s_code VARCHAR(8),
+       fract_owner VARCHAR(1),
+       air_worth_date VARCHAR(8),
+       other_names_1 VARCHAR(50),
+       other_names_2 VARCHAR(50),
+       other_names_3 VARCHAR(50),
+       other_names_4 VARCHAR(50),
+       other_names_5 VARCHAR(50),
+       expiration_date VARCHAR(8),
+       unique_id VARCHAR(8),
+       kit_mfr VARCHAR(30),
+       kit_model VARCHAR(20),
+       mode_s_code_hex VARCHAR(10)
+ );
+
+CREATE INDEX faa_registration_mode_s_code_hex_index on faa_registration (mode_s_code_hex);
