6b68863caa2b5edc8e14658530eb6a202949abec
John Wiseman <jjwiseman@gmail.com>
2015-11-15 10:37:52 -0800

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -3,21 +3,21 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-time "0.9.0"]
+  :dependencies [[clj-time "0.11.0"]
                  [com.lemondronor.leaflet-gorilla "0.1.3"]
-                 [com.lemonodor/gflags "0.7.2"]
-                 [enlive "1.1.5"]
+                 [com.lemonodor/gflags "0.7.3"]
+                 [enlive "1.1.6"]
                  [factual/geo "1.0.0"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.csv "0.1.2"]
+                 [org.clojure/data.csv "0.1.3"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/java.jdbc "0.3.6"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.postgis/postgis-jdbc "1.3.3" :exclusions [postgresql/postgresql]]
-                 [org.xerial/sqlite-jdbc "3.8.7"]
+                 [org.xerial/sqlite-jdbc "3.8.11.2"]
                  [postgresql "9.3-1102.jdbc41"]]
-  :plugins [[lein-gorilla "0.3.4" :exclusions [cider-nrepl]]]
+  :plugins [[lein-gorilla "0.3.5-SNAPSHOT" :exclusions [cider/cider-nrepl]]]
   :main ^:skip-aot com.lemondronor.orbital-detector
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
